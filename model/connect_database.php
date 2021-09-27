35c9108cdbdadfb31501691e53237013fc56a69a
jun0925 <31940359+jun0925@users.noreply.github.com>
2021-09-28 08:45:11 +0900

    MySQL 연결 오류 발생시 프로그램 종료

--- model/connect_database.php
+++ model/connect_database.php
@@ -4,6 +4,6 @@ include $_SERVER['DOCUMENT_ROOT']."/config/env.php";
 // 데이터 베이스 연결
 $conn = mysqli_connect($db['hostname'],$db['username'],$db['password']);
 if(mysqli_connect_errno()){
-    echo "MySQL 연결 오류: ".mysqli_connect_error();
+    die("MySQL 연결 오류: ".mysqli_connect_error());
 }
 ?>
